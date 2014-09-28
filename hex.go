18e2436a7dbf164963e4ecd20abde94f6140c305
Mark Arts <piderman+github@gmail.com>
2014-09-28 20:37:19 +0200

    Split hex.go into multiple files

--- hex.go
+++ hex.go
@@ -1,39 +1,10 @@
 package hex
 
-type HexCell struct {
-	index hexIndex
-}
-
-func (cell *HexCell) GetIndex() hexIndex {
-	return cell.index
-}
-
-func (cell *HexCell) SetIndex(index hexIndex) {
-	cell.index = index
-}
-
 type IndexableHex interface {
 	GetIndex() hexIndex
 	SetIndex(hexIndex)
 }
 
-type HexGrid struct {
-	grid map[hexIndex]IndexableHex
-}
-
 type hexIndex struct {
 	x, y, z int
-}
-
-func NewGrid() HexGrid {
-	return HexGrid{grid: make(map[hexIndex]IndexableHex)}
-}
-
-func (grid *HexGrid) Put(cell IndexableHex, x, y, z int) {
-	cell.SetIndex(hexIndex{x, y, z})
-	(*grid).grid[cell.GetIndex()] = cell
-}
-
-func (grid *HexGrid) Get(x, y, z int) IndexableHex {
-	return (*grid).grid[hexIndex{x, y, z}]
 }
