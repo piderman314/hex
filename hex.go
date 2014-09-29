3205c54b70dafb650f2484f5eedb76e8ca706b15
Mark Arts <piderman+github@gmail.com>
2014-09-29 19:49:22 +0200

    Rename HexCell to Hex

--- hex.go
+++ hex.go
@@ -1,5 +1,22 @@
 package hex
 
+var neighbours = [][]int{
+	{+1, -1, 0}, {+1, 0, -1}, {0, +1, -1},
+	{-1, +1, 0}, {-1, 0, +1}, {0, -1, +1},
+}
+
+type Hex struct {
+	index hexIndex
+}
+
+func (cell *Hex) GetIndex() hexIndex {
+	return cell.index
+}
+
+func (cell *Hex) SetIndex(index hexIndex) {
+	cell.index = index
+}
+
 type IndexableHex interface {
 	GetIndex() hexIndex
 	SetIndex(hexIndex)
--- hexgrid_test.go
+++ hexgrid_test.go
@@ -7,38 +7,36 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("HexGrid", func() {
+var _ = Describe("HexGrid Tests", func() {
 	var (
 		grid HexGrid
 	)
 
 	Describe("HexGrid Get and Put", func() {
-		Describe("When retrieving from the HexGrid", func() {
-			BeforeEach(func() {
-				grid = NewGrid()
-				grid.Put(&cell{}, 1, 1, 1)
-			})
+		BeforeEach(func() {
+			grid = NewGrid()
+			grid.Put(&hex{}, 1, 1, 1)
+		})
 
-			Context("from location 1, 1, 1", func() {
-				It("should not be nil", func() {
-					Expect(grid.Get(1, 1, 1)).NotTo(BeNil())
-				})
+		Context("Getting from a location with a hex", func() {
+			It("should not be nil", func() {
+				Expect(grid.Get(1, 1, 1)).NotTo(BeNil())
 			})
+		})
 
-			Context("from location 2, 2, 2", func() {
-				It("should be nil", func() {
-					Expect(grid.Get(2, 2, 2)).To(BeNil())
-				})
+		Context("Getting from a location without a hex", func() {
+			It("should be nil", func() {
+				Expect(grid.Get(2, 2, 2)).To(BeNil())
 			})
 		})
 	})
 
 	Describe("HexGrid GetAllNeighbours", func() {
 		Context("no neighbours", func() {
-			c := cell{}
+			c := hex{}
 			grid = NewGrid()
 			grid.Put(&c, 1, 1, 1)
-			grid.Put(&cell{}, 3, 3, 3)
+			grid.Put(&hex{}, 3, 3, 3)
 
 			It("should be an empty slice", func() {
 				Expect(len(grid.GetAllNeighbours(&c))).To(Equal(0))
@@ -46,15 +44,15 @@ var _ = Describe("HexGrid", func() {
 		})
 
 		Context("all neighbours", func() {
-			c := cell{}
+			c := hex{}
 			grid := NewGrid()
 			grid.Put(&c, 1, 1, 1)
-			grid.Put(&cell{}, 2, 0, 1)
-			grid.Put(&cell{}, 0, 2, 1)
-			grid.Put(&cell{}, 1, 2, 0)
-			grid.Put(&cell{}, 1, 0, 2)
-			grid.Put(&cell{}, 2, 1, 0)
-			grid.Put(&cell{}, 0, 1, 2)
+			grid.Put(&hex{}, 2, 0, 1)
+			grid.Put(&hex{}, 0, 2, 1)
+			grid.Put(&hex{}, 1, 2, 0)
+			grid.Put(&hex{}, 1, 0, 2)
+			grid.Put(&hex{}, 2, 1, 0)
+			grid.Put(&hex{}, 0, 1, 2)
 
 			It("should be a slice if size 6", func() {
 				Expect(len(grid.GetAllNeighbours(&c))).To(Equal(6))
@@ -63,6 +61,6 @@ var _ = Describe("HexGrid", func() {
 	})
 })
 
-type cell struct {
-	HexCell
+type hex struct {
+	Hex
 }
